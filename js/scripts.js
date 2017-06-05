a9dac2c4e78d366e463a3de608dafa84493ff7af
= <=>
2017-06-05 16:47:58 -0700

    set up basic user survey.

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$(Document).ready(function(){
+  $('#blanks form').submit(function(){
+  });
+  var favoriteColor =$("#color").val();
+  var food-group = $("input:radio[name=food-group]:checked".val();
+  var dob = $("#born").val();
+});
