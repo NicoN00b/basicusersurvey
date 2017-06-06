3ef266301b1a90455f48d0123346e59123a824fb
= <=>
2017-06-05 17:02:35 -0700

    fix js bug.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(Document).ready(function(){
-  $('#blanks form').submit(function(){
+  $('#blanks form').submit(function(event) {
   });
   var favoriteColor =$("#color").val();
   var food-group = $("input:radio[name=food-group]:checked".val();
